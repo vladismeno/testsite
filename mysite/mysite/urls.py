f5aaf561742f03701b0624fe8f0178588c7d4688
vladismeno <vladismeno@gmail.com>
2024-02-28 13:00:14 -0800

    Lesson 6

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# from news.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('news/', index),  # вызов функции index
+    # path('test/', test),
+    path('news/', include('news.urls'))
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    # print(dir(request))
+    return HttpResponse('Hello world')
+
+
+def test(request):
+    return HttpResponse('<h1>Тестовая страница</h1>')
