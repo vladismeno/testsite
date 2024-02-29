743dedbe41ebffeba24d3ec68f0fff809c5aee63
vladismeno <vladismeno@gmail.com>
2024-02-29 15:06:04 -0800

    Lesson 11: Templates

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello world')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context=context)
 
 
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
