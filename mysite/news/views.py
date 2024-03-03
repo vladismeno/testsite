294e1598d3ef829eab1e15403292ee152b73f7d7
vladismeno <vladismeno@gmail.com>
2024-03-02 23:16:10 -0800

    Lesson 21: Обратное разрешение адресов

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import News, Categories
 
 
@@ -20,3 +19,9 @@ def get_category(request, category_id):
         'category': category
     }
     return render(request, template_name='news/category.html', context=context)
+
+
+def view_news(request, news_id):
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {"news_item": news_item})
