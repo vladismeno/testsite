24bc780485a933e729eac69097f7bf158d7cafcc
vladismeno <vladismeno@gmail.com>
2024-03-04 23:35:25 -0800

    Lesson 30: Класс CreateView

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from .models import News, Categories
 from .forms import NewsForm
@@ -52,6 +53,12 @@ class ViewNews(DetailView):
     # pk_url_kwarg = 'news_id'
 
 
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')
+
+
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     category = Categories.objects.get(pk=category_id)
@@ -68,15 +75,15 @@ def get_category(request, category_id):
 #     return render(request, 'news/view_news.html', {"news_item": news_item})
 
 
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            # print(form.cleaned_data)
-            # news = News.objects.create(**form.cleaned_data)
-            news = form.save()
-            return redirect(news)
-
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             # news = News.objects.create(**form.cleaned_data)
+#             news = form.save()
+#             return redirect(news)
+#
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})
