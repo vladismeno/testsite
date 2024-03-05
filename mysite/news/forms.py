c88b523cae2cfeffbad732390660844c8dce228a
vladismeno <vladismeno@gmail.com>
2024-03-04 18:14:02 -0800

    Lesson 24: Работа с формами. Часть 2

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -3,7 +3,20 @@ from .models import Categories
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Categories.objects.all())
+    title = forms.CharField(
+        max_length=150,
+        label='Название',
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    content = forms.CharField(
+        label='Текст',
+        required=False,
+        widget=forms.Textarea(attrs={"class": "form-control", "rows": 5})
+    )
+    is_published = forms.BooleanField(label='Опубликовано?', initial=True)
+    category = forms.ModelChoiceField(
+        queryset=Categories.objects.all(),
+        label='Категория',
+        empty_label='Выберите категорию',
+        widget=forms.Select(attrs={"class": "form-control"})
+    )
