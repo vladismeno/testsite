8d9dc4fc442af2114aff4682f54daa59c4781fbb
vladismeno <vladismeno@gmail.com>
2024-03-04 19:46:36 -0800

    Lesson 26: Кастомные валидаторы

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Categories
 from .models import News
+import re
+from django.core.exceptions import ValidationError
 
 
 # Форма не связанная с моделями
@@ -34,3 +36,8 @@ class NewsForm(forms.ModelForm):
             'category': forms.Select(attrs={"class": "form-control"})
         }
 
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
