0be72392f41f9aef40dd63a1bb6744a401ef9dca
vladismeno <vladismeno@gmail.com>
2024-03-04 19:20:52 -0800

    Lesson 25: Работа с формами. Часть 3

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,22 +1,36 @@
 from django import forms
 from .models import Categories
+from .models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(
-        max_length=150,
-        label='Название',
-        widget=forms.TextInput(attrs={"class": "form-control"})
-    )
-    content = forms.CharField(
-        label='Текст',
-        required=False,
-        widget=forms.Textarea(attrs={"class": "form-control", "rows": 5})
-    )
-    is_published = forms.BooleanField(label='Опубликовано?', initial=True)
-    category = forms.ModelChoiceField(
-        queryset=Categories.objects.all(),
-        label='Категория',
-        empty_label='Выберите категорию',
-        widget=forms.Select(attrs={"class": "form-control"})
-    )
+# Форма не связанная с моделями
+# class NewsForm(forms.Form):
+#     title = forms.CharField(
+#         max_length=150,
+#         label='Название',
+#         widget=forms.TextInput(attrs={"class": "form-control"})
+#     )
+#     content = forms.CharField(
+#         label='Текст',
+#         required=False,
+#         widget=forms.Textarea(attrs={"class": "form-control", "rows": 5})
+#     )
+#     is_published = forms.BooleanField(label='Опубликовано?', initial=True)
+#     category = forms.ModelChoiceField(
+#         queryset=Categories.objects.all(),
+#         label='Категория',
+#         empty_label='Выберите категорию',
+#         widget=forms.Select(attrs={"class": "form-control"})
+#     )
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        # fields = '__all__'
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={"class": "form-control"}),
+            'content': forms.Textarea(attrs={"class": "form-control", "rows": 5}),
+            'category': forms.Select(attrs={"class": "form-control"})
+        }
+
