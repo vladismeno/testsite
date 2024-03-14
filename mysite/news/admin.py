44b7982a4cfd8e8a6e5759e3dc73589723953982
vladismeno <vladismeno@gmail.com>
2024-03-13 20:57:37 -0700

    Lesson 41: Кастомизация админки

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-from .models import News
-from .models import Categories
+from .models import News, Categories
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'get_photo', 'views')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    readonly_fields = ('get_photo', 'created_at', 'updated_at')
+    save_on_top = True  # добавили панель с кнопками вверху страницы
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50px"/>')
+
+    get_photo.short_description = 'Миниатюра'
 
 
 class CategoriesAdmin(admin.ModelAdmin):
@@ -20,3 +29,6 @@ class CategoriesAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Categories, CategoriesAdmin)
+
+admin.site.site_title = 'Управление новостями'
+admin.site.site_header = 'Управление новостями'
--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -29,11 +29,12 @@ class NewsForm(forms.ModelForm):
     class Meta:
         model = News
         # fields = '__all__'
-        fields = ['title', 'content', 'is_published', 'category']
+        fields = ['title', 'content', 'is_published', 'category', 'views']
         widgets = {
             'title': forms.TextInput(attrs={"class": "form-control"}),
             'content': forms.Textarea(attrs={"class": "form-control", "rows": 5}),
-            'category': forms.Select(attrs={"class": "form-control"})
+            'category': forms.Select(attrs={"class": "form-control"}),
+            'views': forms.TextInput(attrs={"class": "form-control"})
         }
 
     def clean_title(self):
