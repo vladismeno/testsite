43730d2c41ef78986fe485fe1214d1663d95f228
vladismeno <vladismeno@gmail.com>
2024-03-14 02:00:06 -0700

    Lesson 42: Миксины

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import News, Categories
 from .forms import NewsForm
+from .utils import MyMixin
 
 
-class HomeNews(ListView):
+class HomeNews(MyMixin, ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
+    mixin_prop = 'hello world'
     # можно добавить это свойство здесь, чтобы оптимизировать запрос
     # queryset = News.objects.select_related('category')
 
@@ -17,14 +20,15 @@ class HomeNews(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
+        context['title'] = self.get_upper('Главная страница')
+        context['mixin_prop'] = self.get_prop()
         return context
 
     def get_queryset(self):
         return News.objects.filter(is_published=True).select_related('category')
 
 
-class NewsByCategory(ListView):
+class NewsByCategory(MyMixin, ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
@@ -32,7 +36,7 @@ class NewsByCategory(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = Categories.objects.get(pk=self.kwargs['category_id'])
+        context['title'] = self.get_upper(Categories.objects.get(pk=self.kwargs['category_id']))
         return context
 
     def get_queryset(self):
@@ -55,10 +59,12 @@ class ViewNews(DetailView):
     # pk_url_kwarg = 'news_id'
 
 
-class CreateNews(CreateView):
+class CreateNews(LoginRequiredMixin, CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
     # success_url = reverse_lazy('home')
+    # login_url = '/admin/'
+    raise_exception = True
 
 
 def get_category(request, category_id):
