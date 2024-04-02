1db7edf7276f01d0e4663187daf16c377c856ac9
vladismeno <vladismeno@gmail.com>
2024-04-01 17:04:00 -0700

    Lesson 70: Многоуровневое меню. Часть 3

--- mysite/testapp/admin.py
+++ mysite/testapp/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
-from .models import Rubric
+from .models import Rubric, Article
+# from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
 
-admin.site.register(Rubric)
+
+class CustomMPTTModelAdmin(DraggableMPTTAdmin):
+    # specify pixel amount for this ModelAdmin only:
+    mptt_level_indent = 20
+
+
+# admin.site.register(Rubric, CustomMPTTModelAdmin)
+admin.site.register(
+    Rubric,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title',
+        # ...more fields if you feel like it...
+    ),
+    list_display_links=(
+        'indented_title',
+    ),
+)
+admin.site.register(Article)
